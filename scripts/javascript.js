1a5d83a5aea3d594b82e344792504bf085943eab
Binary-Daniel <daniel.esteen.edu@rattvik.se>
2020-10-09 14:18:17 +0200

    search+icon

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,23 @@
+
+//search
+function search_scp() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('scp');
+    
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="";                  
+        } 
+    } 
+}
+function search() {
+    var link = document.getElementById('mydiv1');
+    link.style.display = 'none';
+}
 //home buttons
 function Function1() {
     var h = document.getElementById("foo");
