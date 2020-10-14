4f8d8c12f407dc2cdaf896311610a7a06eeec855
Binary-Daniel <daniel.esteen.edu@rattvik.se>
2020-10-14 12:32:00 +0200

    update

--- scripts/license.js
+++ scripts/license.js
@@ -54,3 +54,17 @@ function L11() {
         x.style.display = "block";
     }
 }
+function L13() {
+    var x = document.getElementById("scp-013");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("lic13");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
