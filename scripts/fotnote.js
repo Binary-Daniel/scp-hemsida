bdcbb8749271608edbbfedb95a6335d3b21164b8
Binary-Daniel <daniel.esteen.edu@rattvik.se>
2020-11-12 11:04:53 +0100

    -

--- scripts/fotnote.js
+++ scripts/fotnote.js
@@ -1,12 +1,7 @@
-function show1(F1){
-    document.getElementById("F1").style.visibility = "visible";
+function show(id) {
+    document.getElementById(id).style.visibility = "visible";
 }
-function hide1(F1){
-    document.getElementById("F1").style.visibility = "hidden";
-}
-function show2(F2){
-    document.getElementById("F2").style.visibility = "visible";
-}
-function hide2(F2){
-    document.getElementById("F2").style.visibility = "hidden";
+
+function hide(id) {
+    document.getElementById(id).style.visibility = "hidden";
 }
